bbe91b7714bc947cab89515f2d7e03cfcabe1aae
StefanH <personalnexus@users.noreply.github.com>
2019-12-06 18:49:48 +0100

    day 3: further simplified path generation by moving code into the right places resulting in a single yield added type hints as strings inside class Point overriding __repr__ instead of __str__ for debugging purposes

--- test/day3.py
+++ test/day3.py
@@ -1,24 +1,62 @@
 from test import split, AllLinesTestBase
-from typing import List, Set, Iterable
+from typing import Iterable, List
+
+
+class Point:
+    """
+    An immutable representation of a point on a path. Two points at the same coordinates are
+    considered equal. Points are ordered by their Manhattan distance from the origin (0,0).
+    """
+
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+        self.distanceFromOrigin = abs(self.x) + abs(self.y)
+
+    def __eq__(self, other: 'Point'):
+        return self.x == other.x and self.y == other.y
+
+    def __ne__(self, other: 'Point'):
+        return self.x != other.x or self.y != other.y
+
+    def __hash__(self):
+        return self.distanceFromOrigin
+
+    def __repr__(self):
+        return '[{0}, {1}] {2}'.format(self.x, self.y, self.distanceFromOrigin)
+
+    def __lt__(self, other: 'Point'):
+        return self.distanceFromOrigin < other.distanceFromOrigin
+
+    def __gt__(self, other: 'Point'):
+        return self.distanceFromOrigin > other.distanceFromOrigin
 
 
 class Day3(AllLinesTestBase):
 
-    def process(self, lines):
-        def getPath(line) -> Iterable[Point]:
-            startingPoint = Point(0, 0)
-            turns = split(line)
-            for turn in turns:
-                pathSegment = startingPoint.move(turn)
-                for point in pathSegment:
-                    startingPoint = point
-                    yield point
+    _movementsByDirection = {'R': lambda p: Point(p.x + 1, p.y),
+                             'L': lambda p: Point(p.x - 1, p.y),
+                             'U': lambda p: Point(p.x, p.y + 1),
+                             'D': lambda p: Point(p.x, p.y - 1)}
 
-        path1 = set(getPath(lines[0]))
-        path2 = getPath(lines[1])
+    def process(self, lines):
+        path1 = set(self.getPath(lines[0]))
+        path2 = self.getPath(lines[1])
         intersections = [point for point in path2 if point.distanceFromOrigin != 0 and point in path1]
         minimum = min(intersections)
         return minimum.distanceFromOrigin
+
+    @classmethod
+    def getPath(cls, line) -> Iterable[Point]:
+        point = Point(0, 0)
+        steps = split(line)
+        for step in steps:
+            distance = int(step[1:])
+            if distance != 0:
+                direction = step[0].upper()
+                for _ in range(distance):
+                    point = cls._movementsByDirection[direction](point)
+                    yield point
 
     def testOne(self):
         self.assertEqual(6, self.process(['R8,U5,L5,D3',
@@ -27,45 +65,3 @@ class Day3(AllLinesTestBase):
                                             'U62,R66,U55,R34,D71,R55,D58,R83']))
         self.assertEqual(135, self.process(['R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51',
                                             'U98,R91,D20,R16,D67,R40,U7,R15,U6,R7']))
-
-
-class Point:
-    _movementsByDirection = {'R': lambda p, d: Point(p.X + d, p.Y),
-                             'L': lambda p, d: Point(p.X - d, p.Y),
-                             'U': lambda p, d: Point(p.X, p.Y + d),
-                             'D': lambda p, d: Point(p.X, p.Y - d)}
-
-    def __init__(self, x: int, y: int):
-        self.X = x
-        self.Y = y
-
-    def move(self, turn: str) -> Iterable:
-        direction = turn[0].upper()
-        distance = int(turn[1:])
-        if distance == 0:
-            yield self
-        else:
-            for point in (Point._movementsByDirection[direction](self, d + 1) for d in range(distance)):
-                yield point
-
-    @property
-    def distanceFromOrigin(self):
-        return abs(self.X) + abs(self.Y)
-
-    def __eq__(self, other):
-        return self.X == other.X and self.Y == other.Y
-
-    def __ne__(self, other):
-        return self.X != other.X or self.Y != other.Y
-
-    def __hash__(self):
-        return self.distanceFromOrigin
-
-    def __str__(self):
-        return '[{0}, {1}] {2}'.format(self.X, self.Y, self.distanceFromOrigin)
-
-    def __lt__(self, other):
-        return self.distanceFromOrigin < other.distanceFromOrigin
-
-    def __gt__(self, other):
-        return self.distanceFromOrigin > other.distanceFromOrigin
