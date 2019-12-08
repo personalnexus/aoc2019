724ba46ffdf120e2ed0050b5a2f2ca6a2e015176
StefanH <personalnexus@users.noreply.github.com>
2019-12-08 15:06:11 +0100

    added missing instructions to intcode computer to pass all day 7 example tests fixed type error for handling intcode computer input simplified instructions with two input and one output parameter

--- intcode/__init__.py
+++ intcode/__init__.py
@@ -4,16 +4,16 @@ from intcode.instructions import AbstractMachine
 class IntCodeComputer(AbstractMachine):
 
     def execute(self):
-        i = 0
-        while i < len(self.codes):
+        nextInstruction = 0
+        while nextInstruction < len(self.codes):
             # Code 0: header
-            instructionHeader = str(self.codes[i]).zfill(2)
+            instructionHeader = str(self.codes[nextInstruction]).zfill(2)
             instructionCode = int(instructionHeader[-2:])
             parameterModes = instructionHeader[0:-2]
 
             # Codes 1-n: instruction parameters
-            instruction = instructions.create(instructionCode, parameterModes, i, self)
-            i += 1 + instruction.getParameterCount()
+            instruction = instructions.create(instructionCode, parameterModes, nextInstruction, self)
 
-            if not instruction.execute():
+            nextInstruction = instruction.execute()
+            if nextInstruction is None:
                 break
--- intcode/instructions.py
+++ intcode/instructions.py
@@ -1,6 +1,5 @@
 from abc import ABC
 from typing import List
-
 import intcode.parameters
 
 
@@ -8,7 +7,7 @@ class AbstractMachine(ABC):
 
     def __init__(self, codes: List[int], inputs):
         self.codes = codes
-        self.inputs = inputs if inputs is list else list(inputs)
+        self.inputs = inputs.copy() if isinstance(inputs, list) else [inputs]
         self.hasError = False
         self.output = None
 
@@ -31,31 +30,38 @@ class Instruction(ABC):
         raise NotImplementedError('must implement getParameterCount()')
 
     def execute(self):
-        raise NotImplementedError('must implement execute()')
+        return self.index + 1 + self.getParameterCount()
 
 
-class AddInstruction(Instruction):
-    """Adds parameters one and two and saves result in parameter three"""
-
-    def getParameterCount(self):
-        return 3
-
-    def execute(self):
-        value = self._parameters[0].get() + self._parameters[1].get()
-        self._parameters[2].set(value)
-        return True
-
-
-class MultiplyInstruction(Instruction):
-    """Multiplies parameters one and two and saves result in parameter three"""
+class TwoInOneOutParameterInstruction(Instruction):
+    """Base class for instructions setting a third parameter based on some calculation on two input parameters"""
 
     def getParameterCount(self):
         return 3
 
     def execute(self):
-        value = self._parameters[0].get() * self._parameters[1].get()
+        parameter0 = self._parameters[0].get()
+        parameter1 = self._parameters[1].get()
+        value = self.calculateParameterTwo(parameter0, parameter1)
         self._parameters[2].set(value)
-        return True
+        return super().execute()
+
+    def calculateParameterTwo(self, parameter0: int, parameter1: int) -> int:
+        raise NotImplementedError('must implement calculateParameterTwo()')
+
+
+class AddInstruction(TwoInOneOutParameterInstruction):
+    """Adds parameters zero and one and saves result in parameter two"""
+
+    def calculateParameterTwo(self, parameter0: int, parameter1: int) -> int:
+        return parameter0 + parameter1
+
+
+class MultiplyInstruction(TwoInOneOutParameterInstruction):
+    """Multiplies parameters zero and one and saves result in parameter two"""
+
+    def calculateParameterTwo(self, parameter0: int, parameter1: int) -> int:
+        return parameter0 * parameter1
 
 
 class InputInstruction(Instruction):
@@ -67,7 +73,7 @@ class InputInstruction(Instruction):
     def execute(self):
         value = self.machine.popInput()
         self._parameters[0].set(value)
-        return True
+        return super().execute()
 
 
 class OutputInstruction(Instruction):
@@ -86,7 +92,43 @@ class OutputInstruction(Instruction):
             else:
                 # another non-zero value indicates an error
                 raise RuntimeError('An error occurred before instruction {0}. IO = {1}'.format(self.index, value))
-        return True
+        return super().execute()
+
+
+class JumpIfTrueInstruction(Instruction):
+    """Jump to the instruction given by the value of parameter 1 if parameter 0 is true"""
+
+    def getParameterCount(self):
+        return 3
+
+    def execute(self):
+        result = self._parameters[1].get() if self._parameters[0].get() != 0 else super().execute()
+        return result
+
+
+class JumpIfFalseInstruction(Instruction):
+    """Jump to the instruction given by the value of parameter 1 if parameter 0 is false"""
+
+    def getParameterCount(self):
+        return 3
+
+    def execute(self):
+        result = self._parameters[1].get() if self._parameters[0].get() == 0 else super().execute()
+        return result
+
+
+class LessThanInstruction(TwoInOneOutParameterInstruction):
+    """Return 1 if parameter zero is less than parameter one otherwise return 0"""
+
+    def calculateParameterTwo(self, parameter0: int, parameter1: int) -> int:
+        return 1 if parameter0 < parameter1 else 0
+
+
+class EqualsInstruction(TwoInOneOutParameterInstruction):
+    """Return 1 if parameter zero is equal to parameter one otherwise return 0"""
+
+    def calculateParameterTwo(self, parameter0: int, parameter1: int) -> int:
+        return 1 if parameter0 == parameter1 else 0
 
 
 class BreakInstruction(Instruction):
@@ -99,17 +141,21 @@ class BreakInstruction(Instruction):
         return 0
 
     def execute(self):
-        return False
+        return None
 
 
 _instructionClassesByCode = {1: AddInstruction,
                              2: MultiplyInstruction,
                              3: InputInstruction,
                              4: OutputInstruction,
+                             5: JumpIfTrueInstruction,
+                             6: JumpIfFalseInstruction,
+                             7: LessThanInstruction,
+                             8: EqualsInstruction,
                              99: BreakInstruction}
 
 
-def create(instructionCode: int, parameterModes: str, index: int, machine: AbstractMachine):
+def create(instructionCode: int, parameterModes: str, index: int, machine: AbstractMachine) -> Instruction:
     instructionClass = _instructionClassesByCode[instructionCode]
     result = instructionClass(parameterModes, index, machine)
     return result
