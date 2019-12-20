99769921c732dc0263fca9018d1932a9f1d2e37b
StefanH <personalnexus@users.noreply.github.com>
2019-12-20 06:54:46 +0100

    day19

---
+++ test/day19.py
@@ -0,0 +1,12 @@
+from test import processWithIntCodeComputer, LineByLineTestBase as TestBase
+from typing import List
+
+
+class DayX(TestBase):
+
+    def process(self, line: str):
+        result = sum(processWithIntCodeComputer(line, [x, y])[1][0] for x in range(50) for y in range(50))
+        return result
+
+    def test(self):
+        pass
